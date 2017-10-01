5615e16864d69a364a7d5ab0a83f2ac245d8c9ec
nasatic <ibeokoro@gmail.com>
2017-10-01 22:00:17 +0100

    Test commit

--- src/test/java/Runner/ForecastRunner.java
+++ src/test/java/Runner/ForecastRunner.java
@@ -7,14 +7,13 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(strict = false,
+@CucumberOptions(
         features = "src/test/java/FeatureFiles/forecast.feature",
         glue = "Steps",
         plugin = {"json:target/cucumber.json", "pretty:target/cucumber-pretty.txt","html:target/html/cucumber-html-reports",
        "usage:target/cucumber-usage.json"})
-//        tags = {"@Ibe"})
 
-//public class ForecastRunner extends AbstractTestNGCucumberTests {
+
 public class ForecastRunner {
 
 }
